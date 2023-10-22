a67faff2f36fac688f5b4bb2a5a8c0ec9da5d8a7
adeeshaj <adeesha.14@cse.mrt.ac.lk>
2023-10-22 20:56:18 +0530

    feat: #28 read data from database

---
+++ analyzer.py
@@ -0,0 +1,33 @@
+import psycopg2
+import logging
+import sys
+from dotenv import load_dotenv
+import os
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Load the .env file
+load_dotenv()
+
+DB_URL =  os.environ.get("PSQL_DB_URL")
+table_name="listings"
+
+
+try:
+    conn = psycopg2.connect(DB_URL)
+    cur = conn.cursor()
+
+    query = f"SELECT * FROM {table_name}"
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    logging.info(f"Read {len(rows)} listings from database")
+
+except Exception as e:
+    logging.exception(e)
+
+finally:
+    if 'cur' in locals() and cur:
+        cur.close()
+    if 'conn' in locals() and conn:
+        conn.close()
